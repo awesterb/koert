e9a79efe38ffc0f0ae8911b610bc221767dc13a3
Bram Westerbaan <bram@westerbaan.name>
2011-02-27 14:40:05 +0100

    print-errors.py

---
+++ scripts/print-errors.py
@@ -0,0 +1,26 @@
+from koert.verification.core import Verifier, TrsHaveNum, TrNumsAreRemco, \
+		TrNumsAreRemcoContinuous, AcMutThenNoSplit, TrMutAc, \
+		SpNonZero, TrHaveFin7Softref
+from koert.gnucash.tools import open_gcf
+import koert.verification.fin7scheme as scheme
+import sys
+
+def main(argv):
+	path = " ".join(argv[1:]).strip()
+	if not path:
+		print "please provide a path to a gnucash file as argument."
+		return
+	gcf = open_gcf(path, scheme)
+	book = gcf.fields['books'].values()[0]
+	v = Verifier(gcf)
+	res = v.verify(TrsHaveNum, TrNumsAreRemco, TrNumsAreRemcoContinuous,
+			AcMutThenNoSplit, TrMutAc, SpNonZero, 
+			TrHaveFin7Softref)
+	for fact in res:
+		print fact
+		print res[fact]
+		print ""
+
+if __name__=="__main__":
+	main(sys.argv)
+
