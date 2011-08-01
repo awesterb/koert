82c8c15fadd11500cced73e0c23438e27c3a378b
Bram Westerbaan <bram@westerbaan.name>
2011-08-01 21:47:30 +0200

    balance:  get_balance_history

--- sm/gnucash/balance.py
+++ sm/gnucash/balance.py
@@ -29,3 +29,25 @@ def get_balance_from_muts(book, muts):
 			ac = ac.parent
 	return balance
 
+def get_balance_history(book, trs):
+	date_trs = {}
+	for tr in trs:
+		d = tr.date_posted.date
+		if d not in date_trs:
+			date_trs[d] = []
+		date_trs[d].append(tr)
+	dates = list(date_trs.keys())
+	dates.sort()
+	balance = {}
+	for ac in book.accounts.values():
+		balance[ac] = Decimal(0)
+	for date in dates:
+		balance = dict(balance)
+		for tr in date_trs[date]:
+			for mut in tr.splits.values():
+				ac = mut.account
+				while ac!=None:
+					balance[ac] += mut.value
+					ac = ac.parent
+		yield (date, balance)
+			
